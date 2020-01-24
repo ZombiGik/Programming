96cb41466df95a670f7e2212ad7a7eaf3c33529f
ZombiGik <32338434+ZombiGik@users.noreply.github.com>
2020-01-24 19:08:12 +0300

    Create 25.4.cpp

---
+++ 25.4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	cout << "Введите размер последовательности -> ";
+	int size;
+	cin >> size;
+	int* arr = new int[size];
+	cout << "Введите пследовательность -> ";
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr[i];
+	}
+	bool sequence = false;
+	int counter = 0;
+	for (int i = 0; i < size-1; i++)
+	{
+		if (arr[i] < arr[i + 1])sequence = true;
+		if (sequence == true && arr[i] > arr[i + 1])
+		{
+			counter++;
+			sequence = false;
+		}
+		if (sequence == true && i + 2 == size) counter++;
+	}
+	cout << "Найдено " << counter << " возрастающих последовательностей" << endl;
+	system("pause");
+	return 0;
+}
