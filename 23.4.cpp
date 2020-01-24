88e347ccb5688eca1196fb31ff8ef272caf35c7e
ZombiGik <32338434+ZombiGik@users.noreply.github.com>
2020-01-24 16:38:27 +0300

    Create 23.4.cpp

---
+++ 23.4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(0, "");
+	string words;
+	int KNwords = 0;
+	cout << "Введите строку слов разделённых пробелами -> ";
+	getline(cin, words);
+	cout << words;
+	for (int i = 0; i < words.size(); i++)
+	{
+		if (words[i] != ' ')//защита от неединичного пробела
+		{
+			int Buff = 0;
+			while ((i + Buff < words.size()) && (words[i + Buff] != ' '))
+			{
+				Buff += 1;
+			}
+			cout << Buff << endl;
+			cout << words[i] << ' ' << words[i + Buff - 1] << endl;
+			if (((words[i] == 'к') ||(words[i] == 'К'))&&((words[i + Buff - 1] == 'Н') || (words[i + Buff - 1]=='н')))
+			{
+				KNwords++;
+			}
+			i += Buff;
+		}
+	}
+	cout << "Слов начинающихся на 'к' и заканчивающихся на 'н' найдено: " << KNwords << endl;
+	system("pause");
+	return 0;
+}
